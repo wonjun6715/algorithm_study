aad2b3479369953d3fd1184f323ecc35ce69ad03
wonjun Lee <wj6715@naver.com>
2023-06-19 01:49:58 +0900

    양수인 n의 1부터 n까지 정수의 합

---
+++ chap01/sum1ton_positive.py
@@ -0,0 +1,15 @@
+# 1부터 n까지의 정수의 합 구하기(n값은 양수만 입력받음)
+
+print('1부터 n까지 정수의 합을 구합니다.')
+while True:
+    n = int(input('n값을 입력하세요: '))
+    if n > 0:
+        break # n값이 양수이면 무한루프 빠져나옴
+    
+sum = 0
+
+for i in range(1, n + 1):
+    sum += i
+
+print(f'1부터 {n}까지의 정수의 합은 {sum}입니다.')
+
