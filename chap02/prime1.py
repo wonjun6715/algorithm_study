0584cfae1c48b78e70cdc4c94ef7d7e52a140c63
wonjun Lee <wj6715@naver.com>
2023-06-24 15:25:35 +0900

    소수로 나열하기 방법1

--- chap02/prime1.py
+++ chap02/prime1.py
@@ -1,13 +1,13 @@
 # 1000이하의 소수를 나열하기
 
-counter = 0
+counter = 0 # 나눗셈 횟수 카운트
 
 for n in range(2,1001):
     for i in range(2, n):
         counter+=1
-        if n % i == 0:
-            break
-        else:
-            print(n)
+        if n % i == 0: # 나누어 떨어지면 소수가 아님
+            break #  반복은 더 이상 불필요하여 중단
+    else:
+            print(n) # 끝까지 나누어 떨어지지 않으면 다음 수행
             
 print(f'나눗셈을 실행한 횟수: {counter}')
